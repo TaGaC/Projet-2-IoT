2e2e358cf253a0239242b190074b1a8d223b988f
MizuSARiUs <thomas.lebaron@etu.univ-tours.fr>
2025-04-12 23:29:49 -0400

    login et dashboard

--- fake-iot/app.py
+++ fake-iot/app.py
@@ -1,18 +1,32 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session, url_for
 
 app = Flask(__name__)
-password = "SuperSecret123"
+app.secret_key = "UneSuperCleSecrete"  # Nécessaire pour les sessions
+
+# Identifiants d'accès
+USERNAME = "admin"
+PASSWORD = "SuperSecret123"
 
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        if request.form.get("password") == password:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username == USERNAME and password == PASSWORD:
+            session["user"] = username
             return redirect("/dashboard")
     return render_template("login.html")
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+    if "user" not in session:
+        return redirect(url_for("login"))
+    return render_template("dashboard.html", username=session["user"])
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
